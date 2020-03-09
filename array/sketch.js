dca4a57fdd24aacf60dcfd51aa2ac0a25b4c1e00
dandreao16 <47123811+dandreao16@users.noreply.github.com>
2020-03-09 17:35:33 -0400

    up

--- array/sketch.js
+++ array/sketch.js
@@ -44,6 +44,11 @@ function setup() {
     
 
 }
+textSize(18);
+	fill('black');
+	// text("Click to go to the next scene", width - 100, height - 70, 100);
+	text("click for sound", width - 100, height - 70, 100);
+}
 
 function mousePressed() {
   mySound.play();
